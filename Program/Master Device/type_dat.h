950988b60884e173cca5c6b9b09a43f196ddbb91
Jayanth S Balan <jayanthsb.005@gmail.com>
2025-11-21 19:55:13 +0530

    Add files via upload

---
+++ Program/Master Device/type_dat.h
@@ -0,0 +1,17 @@
+
+#ifndef TYPE_DAT_H
+#define TYPE_DAT_H
+
+#include <Arduino.h>
+#include <vector>
+using namespace std;
+
+//TS Object
+struct sigpoints
+{
+    uint16_t xpos;
+    uint16_t ypos;
+    uint16_t zpos;
+};
+
+#endif
