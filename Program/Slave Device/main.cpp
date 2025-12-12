dba6626fd323e51ee11d41aa06d57ed93013bf69
Jayanth S Balan <jayanthsb.005@gmail.com>
2025-12-12 20:01:51 +0530

    Refactor KNN classification and task management
    Refactor and enhance task management for KNN classification and training processes.

--- Program/Slave Device/main.cpp
+++ Program/Slave Device/main.cpp
@@ -7,8 +7,16 @@
 TaskHandle_t read_dat_handle = NULL;
 TaskHandle_t vec_process_handle = NULL;
 TaskHandle_t wait_task_handle = NULL;
+TaskHandle_t train_proc_1_handle = NULL;
+TaskHandle_t train_proc_2_handle = NULL;
+TaskHandle_t class_task1_handle = NULL;
+TaskHandle_t class_task2_handle = NULL;
 QueueHandle_t sig_transfer_Q = NULL;
+QueueHandle_t sig_transfer_Q1 = NULL;
+QueueHandle_t sig_transfer_Q2 = NULL;
 SemaphoreHandle_t sign_lock;
+
+KNNClassifier signatureKNN(KNN_FEATURES);
 
 void wait_task(void* param)
 {
@@ -38,37 +46,42 @@ void vec_process(void* param) {
     
     //DTW Results
     vector<float>* dtw_new = nullptr;
-    vector<vector<float>> train_pass, train_fail;
+    float features1[KNN_FEATURES], features2[KNN_FEATURES];
     
     //Training Data
     vector<float>* train_pattern = nullptr;
-    int train_count = 0;
+    int train_count1 = 0, train_count2 = 0;
     vTaskDelay(10/portTICK_PERIOD_MS);
-    while(train_count < df_pass)
+
+    //Train all patterns
+    Serial.println("knn::: Training model");
+    Serial.print("knn::: Model trained with ");
+    Serial.print(signatureKNN.getCount());
+    Serial.println(" examples");
+    while(train_count1 < df_pass || train_count2 < df_fail)
     {
-      if(xQueueReceive(sig_transfer_Q, &train_pattern, pdMS_TO_TICKS(10000)) == pdPASS) {
-        train_count++;
+      if(xQueueReceive(sig_transfer_Q1, &train_pattern, pdMS_TO_TICKS(10000)) == pdPASS) {
+        train_count1++;
         Serial.print("proc::: Received training pattern (");
         Serial.print(train_pattern->size());
         Serial.println(" values)");
-        
-        train_pass.push_back(*train_pattern);
+
+        extract_features(*train_pattern, features1);
+        signatureKNN.addExample(features1, GENUINE_CLASS);
+
         delete train_pattern;
         
         vTaskDelay(pdMS_TO_TICKS(10));
       }
-    }
-    
-    train_count = 0;
-    while(train_count < df_fail)
-    {
-      if(xQueueReceive(sig_transfer_Q, &train_pattern, pdMS_TO_TICKS(10000)) == pdPASS) {
-        train_count++;
+      if(xQueueReceive(sig_transfer_Q2, &train_pattern, pdMS_TO_TICKS(10000)) == pdPASS) {
+        train_count2++;
         Serial.print("proc::: Received training pattern (");
         Serial.print(train_pattern->size());
         Serial.println(" values)");
         
-        train_fail.push_back(*train_pattern);
+        extract_features(*train_pattern, features2);
+        signatureKNN.addExample(features2, FORGERY_CLASS);
+
         delete train_pattern;
         
         vTaskDelay(pdMS_TO_TICKS(10));
@@ -76,13 +89,13 @@ void vec_process(void* param) {
     }
     
     Serial.print("\nproc::: Total training patterns = ");
-    Serial.println(train_pass.size() + train_fail.size());
+    Serial.println(train_count1 + train_count2);
 
     ulTaskNotifyTake(pdTRUE, portMAX_DELAY); // Wait for Queue
 
     //New Data
     if(xQueueReceive(sig_transfer_Q, &dtw_new, pdMS_TO_TICKS(5000)) != pdPASS) {
-      Serial.println("proc::: Error - No DTW data received");
+      Serial.println("proc::: Error - No DTW data received in Queue 1");
       continue;
     }
     
@@ -99,12 +112,12 @@ void vec_process(void* param) {
     
     //Classify
     float con;
-    bool knn_result = knn_classify(&con, *dtw_new, train_pass, train_fail, train_pass.size(), train_fail.size());
+    bool knn_result = knn_classify(&con, *dtw_new, train_count1, train_count2);
     
     delete dtw_new;  //Cleanup
 
     noInterrupts();
-    if(knn_result) {
+    if(knn_result && con > CONFIDENCE_THRESHOLD) {
       a = 'y';
       b = '1';
     } else {
@@ -136,11 +149,14 @@ void vec_process(void* param) {
 
 void setup() {
   Serial.begin(115200);
+  esp_task_wdt_init(20, true);
   delay(500);
   Serial.println("\nSystem Boot");
 
-  sig_transfer_Q = xQueueCreate(20, sizeof(vector<float>*));
-  if(sig_transfer_Q == NULL) {
+  sig_transfer_Q = xQueueCreate(1, sizeof(vector<float>*));
+  sig_transfer_Q1 = xQueueCreate(20, sizeof(vector<float>*));
+  sig_transfer_Q2 = xQueueCreate(20, sizeof(vector<float>*));
+  if(sig_transfer_Q == NULL || sig_transfer_Q1 == NULL || sig_transfer_Q2 == NULL) {
     Serial.println("Error: Failed to create queue");
     while(1);
   }
@@ -169,7 +185,7 @@ void setup() {
   if(Serial.available())
   {
     char command = Serial.read();
-    if(command == 'f')
+    if(command == 'f') //Append as Forgery
     {
       while(!tx_fin)
       {
@@ -182,7 +198,7 @@ void setup() {
       delay(10);
       ESP.restart();
     }
-    else if(command == 'p')
+    else if(command == 'p') //Append as Genuine
     {
       while(!tx_fin)
       {
@@ -195,13 +211,13 @@ void setup() {
       delay(10);
       ESP.restart();
     }
-    else if(command == 't')
+    else if(command == 't') //Performance Test
     {
       performance_check();
       delay(10000);
       ESP.restart();
     }
-    else if(command == 'x')
+    else if(command == 'x') //Restart and clear data
     {
       clear_dat();
       delay(10);
@@ -212,9 +228,13 @@ void setup() {
     }
   }
 
-  xTaskCreatePinnedToCore(wait_task, "Input_Sign_Wait", 4000, NULL, 1, &wait_task_handle, 0);
-  xTaskCreatePinnedToCore(read_dat, "Read_SD_File", 12000, NULL, 2, &read_dat_handle, 0);
+  xTaskCreatePinnedToCore(wait_task, "Input_Sign_Wait", 4000, NULL, 1, &wait_task_handle, 1);
+  xTaskCreatePinnedToCore(read_dat, "Read_SD_File", 12000, NULL, 2, &read_dat_handle, 1);
   xTaskCreatePinnedToCore(vec_process, "Vector_Process", 15000, NULL, 2, &vec_process_handle, 0);
+  xTaskCreatePinnedToCore(train_proc_1, "Training_Process_1", 12000, NULL, 3, &train_proc_1_handle, 0);
+  xTaskCreatePinnedToCore(train_proc_2, "Training_Process_2", 12000, NULL, 3, &train_proc_2_handle, 1);
+  xTaskCreatePinnedToCore(class_task1, "Classification_Task_1", 8000, NULL, 3, &class_task1_handle, 0);
+  xTaskCreatePinnedToCore(class_task2, "Classification_Task_2", 8000, NULL, 3, &class_task2_handle, 1);
 
   Serial.println("Tasks created");
   Serial.println("System Ready\n");
@@ -229,9 +249,10 @@ void setup() {
   Serial.print(a);
   Serial.println(b);
 
-  performance_check();
+  dataset_trim(false);
+  dataset_trim(true);
 
-  delay(2000);
+  delay(3000);
   ESP.restart();
 }
 
