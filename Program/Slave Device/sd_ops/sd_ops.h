a89601f849f8110c4bfc4455671f4f51aa253121
Jayanth S Balan <jayanthsb.005@gmail.com>
2025-12-12 20:03:38 +0530

    Update sd_ops.h

--- Program/Slave Device/sd_ops/sd_ops.h
+++ Program/Slave Device/sd_ops/sd_ops.h
@@ -12,8 +12,13 @@
 //Function Prototypes
 void append_dat(const vector<sigpoints>&, bool);
 void read_dat(void*);
+void train_proc_1(void*);
+void train_proc_2(void*);
+void class_task1(void*);
+void class_task2(void*);
 void sd_init();
 void clear_dat();
+void dataset_trim(bool);
 void performance_check();
 
 //Global Variables
