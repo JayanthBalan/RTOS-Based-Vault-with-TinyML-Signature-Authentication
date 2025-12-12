1985df24fadd55abc0317ab3f69bf7733e6191d6
Jayanth S Balan <jayanthsb.005@gmail.com>
2025-12-12 20:04:42 +0530

    Refactor knn_dtw_ops.h with new features and thresholds
    Updated function prototypes and configurations.

--- Program/Slave Device/knn_dtw_ops/knn_dtw_ops.h
+++ Program/Slave Device/knn_dtw_ops/knn_dtw_ops.h
@@ -4,21 +4,20 @@
 
 //Libraries
 #include "type_dat.h"
-#include <Arduino_KNN.h>
 #include <cmath>
 #include <algorithm>
 
 //Function Prototypes
 float dtw_distance(const vector<sigpoints>&, const vector<sigpoints>&);
 void knn_init();
-bool knn_classify(float*, const vector<float>&, const vector<vector<float>>&, const vector<vector<float>>&, int, int);
+bool knn_classify(float*, const vector<float>&, int, int);
+void extract_features(const vector<float>&, float[]);
 
 //Classifier Configurations
-#define KNN_FEATURES 5
 #define KNN_K_VALUE 3
 #define GENUINE_CLASS 1
 #define FORGERY_CLASS 0
-#define CONFIDENCE_THRESHOLD 0.3
+#define CONFIDENCE_THRESHOLD 0.5
 
 //Global
 #define INF 999999.0
