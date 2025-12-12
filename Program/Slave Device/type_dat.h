7f117496af857da7a07ea777318debabf7d8d6a8
Jayanth S Balan <jayanthsb.005@gmail.com>
2025-12-12 20:01:21 +0530

    Enhance type_dat.h with KNN and additional handles

--- Program/Slave Device/type_dat.h
+++ Program/Slave Device/type_dat.h
@@ -4,6 +4,10 @@
 
 #include <Arduino.h>
 #include <vector>
+#include <Arduino_KNN.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_task_wdt.h"
 using namespace std;
 
 //Signature Object
@@ -17,12 +21,21 @@ struct sigpoints
 extern vector<sigpoints> sign;
 extern int df_pass, df_fail;
 
+#define KNN_FEATURES 5
+
 //Global Task handles
 extern TaskHandle_t read_dat_handle;
 extern TaskHandle_t vec_process_handle;
+extern TaskHandle_t train_proc_1_handle;
+extern TaskHandle_t train_proc_2_handle;
+extern KNNClassifier signatureKNN;
+extern TaskHandle_t class_task1_handle;
+extern TaskHandle_t class_task2_handle;
 
 //Global Queue handle
 extern QueueHandle_t sig_transfer_Q;
+extern QueueHandle_t sig_transfer_Q1;
+extern QueueHandle_t sig_transfer_Q2;
 
 //Global Mutex handle
 extern SemaphoreHandle_t sign_lock;
